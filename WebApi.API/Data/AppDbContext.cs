4cc27be158cbdc064d9d336b589ceb979483f4bb
PrafullBendale <bendale1prafull@gmail.com>
2024-03-11 15:34:38 +0530

    Domain Model creationn and database connection using ef core

---
+++ WebApi.API/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.API.Models.Domain;
+
+namespace WebApi.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk>  Walks { get; set; }
+    }
+}
